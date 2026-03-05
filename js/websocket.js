b0ccff3bf2d887479f2b859341411e5f8649e633
Josiah <josiah.wilke77@outlook.com>
2026-03-04 19:00:40 -0600

    Update websocket.js

--- js/websocket.js
+++ js/websocket.js
@@ -1,4 +1,4 @@
-export const socket = new WebSocket("wss://YOUR_SERVER");
+export const socket = new WebSocket("wss://https://war-zone-x.onrender.com");
 
 socket.onmessage = (msg) => {
   const data = JSON.parse(msg.data);
