4de748a5e5b315edbdc91fc34e2d26d1b78c64f0
Josiah <josiah.wilke77@outlook.com>
2026-03-04 18:50:49 -0600

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 export const supabase = createClient(
-  "SUPABASE_URL",
-  "xrudwwgkblhdtddvssst"
+  "https://xrudwwgkblhdtddvssst.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhydWR3d2drYmxoZHRkZHZzc3N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NDc5OTMsImV4cCI6MjA4NzUyMzk5M30.A4v0PFXwuLXox79l2GQlLd000xdGGRiu3le9C78-LJc"
 );
